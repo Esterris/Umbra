c1e3c65bf8cc447f03cd1ae1ac505acc3b7ae13a
Devery <dlturcotte@gmail.com>
2019-11-14 02:00:47 -0500

    Engine entry point created

---
+++ Sandbox/SandboxApp.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Umbra.h"
+
+class SandboxApp : public Umbra::Application
+{
+
+public:
+
+	SandboxApp(){}
+
+	~SandboxApp(){}
+};
+
+Umbra::Application* Umbra::CreateApplication()
+{
+	return new SandboxApp();
+}
+
